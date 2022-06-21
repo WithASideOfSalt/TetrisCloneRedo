3e4642c9f335948815f104b1691ab81030fa6aa7
Scott <scottmcbridetd@gmail.com>
2022-06-21 21:37:34 +0100

    General start of form with play area

--- Tetris Clone Redo/Form1.cs
+++ Tetris Clone Redo/Form1.cs
@@ -12,9 +12,41 @@ namespace Tetris_Clone_Redo
 {
     public partial class Form1 : Form
     {
+        Timer time = new Timer();
+        playPoint[,] playArea;
+        PictureBox PlayAreaDisplay;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region prepare form
+            //modifies the form to correct size
+            Size = new Size(700, 700);
+            BackColor = Color.Gainsboro;
+            CenterToScreen();
+            #endregion
+
+            #region GetPlayArea
+            //self explanitory
+            playArea = new playPoint[10, 24];
+            for (int c = 0; c< 10; c++)
+            {
+                Parallel.For(0, 24, i =>
+                {
+                    playArea[c, i] = new playPoint(c, i);
+                });
+            }
+            PlayAreaDisplay = new PictureBox()
+            {
+                Location = playArea[0, 4].Location,
+                Size = new Size(250, 500),
+                BackColor = Color.AliceBlue
+            };
+            Controls.Add(PlayAreaDisplay);
+            #endregion
         }
     }
 }
---
+++ Tetris Clone Redo/playPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Tetris_Clone_Redo
+{
+    internal class playPoint
+    {
+        public Point Location;
+        public bool Occupied;
+        public playPoint(int x, int y)
+        { 
+            Location = new Point(75+(x*25),(y*25)-25);
+            Occupied = false;
+
+        }
+    }
+}
